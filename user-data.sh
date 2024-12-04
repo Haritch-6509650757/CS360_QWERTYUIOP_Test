7f3eab4b645b2639d95981eae6b2f7a42c94d870
Haritch-6509650757 <144800381+Haritch-6509650757@users.noreply.github.com>
2024-12-04 19:06:06 +0700

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -3,12 +3,14 @@
 sudo yum update -y
 sudo yum install -y docker
 # Start docker serivce
-sudo service docker start
+sudo systemctl start docker
 # Add ec2-user to the docker group
 sudo usermod -aG docker ec2-user
+# Apply group changes without logout
+newgrp docker
 #create necessary directories for Strapi
 sudo mkdir -P /app/public/uploads
 sudo chmod -R 775 /app/public/uploads
 
 # Restart docker service to apply change
-sudo service docker restart
+sudo systemctl restart docker
